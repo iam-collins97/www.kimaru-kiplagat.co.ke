3821f4234a9b212b4b47cc780c77c11fa9575a5f
Collins Shiundu <cshiundu97@gmail.com>
2026-09-25 13:15:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const hamburger = document.querySelector("#hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", function () {
+    navLinks.classList.toggle("nav-open");
+});
 function typeText(element, startDelay) {
     const fullText = element.textContent;
     element.textContent = "";
